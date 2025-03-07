daad13cfa209f4c1061ee181adced67e09d3c3dd
Daniel Millson <danielmillson923@gmail.com>
2025-03-07 14:37:24 -0500

    Completed problems 114 and 115

--- 101-150/Problem115/prob115.py
+++ 101-150/Problem115/prob115.py
@@ -1,9 +1,9 @@
 LENGTH = 50
-MIN_BLOCK_SIZE = 3
+MIN_BLOCK_SIZE = 50
 
 GAP_CACHE = {}
 
-def solution(remainingSpaces):
+def solution_two(remainingSpaces):
     if remainingSpaces < MIN_BLOCK_SIZE:
         return 0
     
@@ -19,14 +19,17 @@ def solution(remainingSpaces):
             
 
             next_position = position + blockSize + 1
-            total_combinations += solution(remainingSpaces - next_position)
+            total_combinations += solution_two(remainingSpaces - next_position)
             
     GAP_CACHE[remainingSpaces] = total_combinations
     return total_combinations
 
 def main():
+    global LENGTH
     # start with 1 for the vacuous case
-    print(1 + solution(LENGTH))
+    while (solutionCount := 1 + solution_two(LENGTH)) < 10**6:
+        LENGTH += 1
+    print(LENGTH)
 
 if __name__ == "__main__":
     main()
